18465e52f99d282642839eae48b1e938dd050d53
rajshreesathawane01 <rajshreesathawane9@gmail.com>
2021-04-21 16:11:55 +0530

    added MoviePy

---
+++ MoviePy_Demo.py
@@ -0,0 +1,14 @@
+# from moviepy.editor import *
+# clip = VideoFileClip("C:\\Users\\hp\\Desktop\\abc.MP4").rotate(180)
+# clip.ipython_display(width = 280)
+
+
+#print("Print ASCII code Z :",ord('z'))
+
+print("Print ASCII code a :",ord('a'))
+print("Print ASCII code A :",ord('A'))
+if('a'>'A'):
+    print("print a greater that A")
+else:
+    print("print A greater that a")
+
